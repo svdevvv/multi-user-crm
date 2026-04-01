cfbea0f5e1abb64e374552430307bcee1fd27849
svdevvv <sv.devvv@gmail.com>
2026-04-01 23:34:07 +0300

    second entity + mapping + edit cfg

--- service/src/main/java/com/svdev/entity/Client.java
+++ service/src/main/java/com/svdev/entity/Client.java
@@ -2,12 +2,11 @@ package com.svdev.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,21 +19,24 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "\"user\"")
-public class User {
+public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     private String name;
     private String email;
-    private String password;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "company_name")
+    private String companyName;
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
-
-
 }
